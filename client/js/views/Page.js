79e29c4ff82990fc6a41f127c2ba4ba1f6bb5869
cbaron <topherbaron@gmail.com>
2016-02-28 22:39:42 -0500

    lets do it

--- client/js/views/Page.js
+++ client/js/views/Page.js
@@ -17,6 +17,20 @@ Object.assign( Page.prototype, {
         } ) 
 
         this.body.find('[data-js="rehearsalLink"]').click( () => this.body.animate( { scrollTop: this.content.rehearsal.position().top }, 1000 ) )
+
+        this.$('#mobile-menu').click( e => {
+            this.$('#nav-list').css( { display: 'block' } )
+            this.$('#mobile-menu').css( { display: 'none' } )
+            e.stopImmediatePropagation()
+            this.$( document ).on( 'click', () => {
+                this.$('#mobile-menu').css( { display: 'inline-block' } )
+                this.$('#nav-list').css( { display: 'none' } )
+            } )
+        } )
+
+        if( this.$(window).width() <= 785 ) {
+            this.$('iframe').attr( { src: this.$('iframe').attr('src').slice( 0, -1 ) + '3' } )
+        }
         
         return this
     }
