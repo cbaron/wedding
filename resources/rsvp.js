05fc8f5826f6e4a1d58021d94c0580b99c3cc775
cbaron <topherbaron@gmail.com>
2016-07-07 11:06:15 -0400

    updated email and gzipped bundle

--- resources/rsvp.js
+++ resources/rsvp.js
@@ -10,8 +10,8 @@ Object.assign( RSVP.prototype, Base.prototype, {
             [ 'names', 'accepts', 'number', 'veg', 'type' ].forEach( key => { if( this.body[ key ] === undefined ) throw new Error("Sorry Mate") } )
 
             return this.Q( this.Email.send( {
-                to: 'topherbaron@gmail.com',
-                from: 'cbaron@alexgandchrisb.com',
+                to: 'rsvp@alexgandchrisb@gmail.com',
+                from: 'root@alexgandchrisb.com',
                 subject: `${this.body.type} RSVP`,
                 body: this.generateEmailBody() } )
             )
